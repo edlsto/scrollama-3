6872533187352a2da04fda80d48ca3c67e511e12
Ed Stoner <edwardlstoner@gmail.com>
2019-05-07 16:47:46 -0600

    one chart

--- charts/chart2.js
+++ charts/chart2.js
@@ -8,12 +8,6 @@ var svg1 = d3.select('#one')
   .attr('width', width + margin.left + margin.right)
   .attr('height', height + margin.top + margin.bottom)
 
-var svg2 = d3.select('#two')
-  .append('svg')
-
-var svg3 = d3.select('svg#three')
-var svg4 = d3.select('svg#four')
-
 
 
 
@@ -46,7 +40,7 @@ dates: columns
   
   xAxis = g => g
     .attr("transform", `translate(0,${height - margin.bottom})`)
-    .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0).tickFormat(d3.timeFormat("%b")))
+    .call(d3.axisBottom(x).ticks(width / 100).tickSizeOuter(0).tickFormat(d3.timeFormat("%b")))
 ;
     yAxis = g => g
     .attr("transform", `translate(${margin.left},0)`)
@@ -59,8 +53,10 @@ dates: columns
         .text(data.y))
 
   svg1.append("g")
+  .style("font", "16px times")
       .call(xAxis);
   svg1.append("g")
+  .style("font", "16px times")
       .call(yAxis);
 
   var path = svg1.append("g")
@@ -90,347 +86,36 @@ dates: columns
     dot.attr("transform", "translate(610.5012376237623,51.5)")
     .attr("id", "thisyear")
     dot.select("text").text("2018-19")
-  svg1.call(hover, path);
+  svg1.call(path);
   return svg1.node();
 
-function hover(svg, path) {
-  svg
-      .style("position", "relative");
-  if ("ontouchstart" in document) svg
-      .style("-webkit-tap-highlight-color", "transparent")
-      .on("touchmove", moved)
-      .on("touchstart", entered)
-      .on("touchend", left)
-  else svg
-      .on("mousemove", moved)
-      .on("mouseenter", entered)
-      .on("mouseleave", left);
-  const dot = svg.append("g")
-      .attr("display", "none");
-  dot.append("circle")
-      .attr("r", 2.5);
-  dot.append("text")
-      .style("font", "10px sans-serif")
-      .attr("text-anchor", "middle")
-      .attr("y", -8);
-  function moved() {
-    d3.event.preventDefault();
-    const ym = y.invert(d3.event.layerY);
-    const xm = x.invert(d3.event.layerX);
-    const i1 = d3.bisectLeft(data.dates, xm, 1);
-    const i0 = i1 - 1;
-    const i = xm - data.dates[i0] > data.dates[i1] - xm ? i1 : i0;
-    const s = data.series.reduce((a, b) => Math.abs(a.values[i] - ym) < Math.abs(b.values[i] - ym) ? a : b);
-    path.attr("stroke", d => d === s ? 'steelblue' : "#ddd").filter(d => d === s).raise();
-    dot.attr("transform", `translate(${x(data.dates[i])},${y(s.values[i])})`);
-    dot.select("text").text(s.name)
-    d3.select('#thisyear').remove()
-  }
-  function entered() {
-    path.style("mix-blend-mode", null).attr("stroke", "#ddd");
-    dot.attr("display", 'null');
-  }
-  function left() {
-    path.style("mix-blend-mode", "multiply").attr("stroke", null);
-    dot.attr("transform", "translate(610.5012376237623,51.5)")
-      d3.select('path#year0').attr("stroke", "steelblue")
-    dot.select("text").text("2018-19")
-  }
-}
 
-   // Define responsive behavior
+
+   function responsivefy(svg) {
+    // get container + svg aspect ratio
+    var container = d3.select(svg.node().parentNode),
+        width = parseInt(svg.style("width")),
+        height = parseInt(svg.style("height")),
+        aspect = width / height;
+
+    // add viewBox and preserveAspectRatio properties,
+    // and call resize so that svg resizes on inital page load
+    svg.attr("viewBox", "0 0 " + width + " " + height)
+        .attr("perserveAspectRatio", "xMinYMid")
+        .call(resize);
+
+    // to register multiple listeners for same event type, 
+    // you need to add namespace, i.e., 'click.foo'
+    // necessary if you call invoke this function for multiple svgs
+    // api docs: https://github.com/mbostock/d3/wiki/Selections#on
+    d3.select(window).on("resize." + container.attr("id"), resize);
+
+    // get width of container and resize svg to fit it
     function resize() {
-      var width = parseInt(d3.select("#one").style("width")) - margin.left - margin.right,
-      height = parseInt(d3.select("#one").style("height")) - margin.top - margin.bottom;
-
-      // Update the range of the scale with new width/height
-      xScale.range([0, width]);
-      yScale.range([height, 0]);
-
-      // Update the axis and text with the new scale
-      svg.select('.x.axis')
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-
-      svg.select('.y.axis')
-        .call(yAxis);
-
-      // Force D3 to recalculate and update the line
-      svg.selectAll('.line')
-        .attr("d", function(d) { return line(d.datapoints); });
-
-      // Update the tick marks
-      xAxis.ticks(Math.max(width/75, 2));
-      yAxis.ticks(Math.max(height/50, 2));
-
-    };
-
-    // Call the resize function whenever a resize event occurs
-    d3.select(window).on('resize', resize);
-
-    // Call the resize function
-    resize();
+        var targetWidth = parseInt(container.style("width"));
+        svg.attr("width", targetWidth);
+        svg.attr("height", Math.round(targetWidth / aspect));
+    }
+}
 
 })
-
-d3.tsv("https://gist.githubusercontent.com/edlsto/8970422bd7c7c135cdaad05470d7f03d/raw/91771879c78a8dc80bf58e7e07b92b494868f77f/gistfile1.tsv", function(error, data2) {
-  if (error) throw error;
-
-
-
-  // format the data
-  data2.forEach(function(d) {
-    d.inches = +d.inches;
-  });
-
-    var data2 =  {
-    y: "# inches",
-series: data2
-}
-
-
-  var x = d3.scaleBand()
-    .domain(data2.series.map(d => d.name))
-    .range([margin.left, width - margin.right])
-    .padding(0.1)
-var y = d3.scaleLinear()
-    .domain([0, d3.max(data2.series, d => d.inches)]).nice()
-    .range([height - margin.bottom, margin.top])
-
-
-  // append the rectangles for the bar chart
-  svg2.append("g")
-      .attr("fill", "lightgray")
-    .selectAll("rect")
-    .data(data2.series)
-    .enter()
-    .append('rect')
-      .attr("x", d => x(d.name))
-      .attr("y", d => y(d.inches))
-      .attr("height", d => y(0) - y(d.inches))
-      .attr("width", x.bandwidth())
-      .attr("id", (d, i) => {
-      return "year" + i
-      })
-        d3.select('rect#year9').attr("fill", "steelblue")
-
-
-  // add the x Axis
-xAxis = g => g
-    .attr("transform", `translate(0,${height - margin.bottom})`)
-    .call(d3.axisBottom(x).tickSizeOuter(0))
-
-yAxis = g => g
-    .attr("transform", `translate(${margin.left},0)`)
-    .call(d3.axisLeft(y))
-    .call(g => g.select(".domain").remove())
-        .call(g => g.select(".tick:last-of-type text").clone()
-        .attr("x", 3)
-        .attr("text-anchor", "start")
-        .attr("font-weight", "bold")
-        .text(data2.y))
-
-  svg2.append("g")
-      .call(xAxis);
-  
-  svg2.append("g")
-      .call(yAxis);
-
-
-});
-
-
-d3.tsv("https://gist.githubusercontent.com/edlsto/05fef0d61318a5b91f8d475216fba034/raw/e1787709132262fb0890bc33b6cdcfd8fd83fbf1/daily-new-snow.tsv", function(error, data3) {
-  if (error) throw error;
-
-
-  // format the data
-  data3.forEach(function(d) {
-    d.inches = +d.inches,
-    d.date = parseTime(d.date)
-
-  });
-
-  var dates = data3.map(
-    d => d.date
-  )
-
-
-    var data3 =  {
-    y: "# inches",
-series: data3,
-dates: dates
-}
-
-
-  var x = d3.scaleTime()
-    .domain(d3.extent(data3.dates))
-    .range([margin.left, width - margin.right])
-
-var y = d3.scaleLinear()
-    .domain([0, 15])
-    .range([height - margin.bottom, margin.top])
-
-  // append the rectangles for the bar chart
-  svg3.append("g")
-      .attr("fill", "steelblue")
-    .selectAll("rect")
-    .data(data3.series)
-    .enter()
-    .append('rect')
-      .attr("x", d => x(d.date))
-      .attr("y", d => y(d.inches))
-      .attr("height", d => y(0) - y(d.inches))
-      .attr("width", 5)
-
-
-
-  // add the x Axis
-xAxis = g => g
-    .attr("transform", `translate(0,${height - margin.bottom})`)
-    .call(d3.axisBottom(x).tickSizeOuter(0))
-
-yAxis = g => g
-    .attr("transform", `translate(${margin.left},0)`)
-    .call(d3.axisLeft(y))
-    .call(g => g.select(".domain").remove())
-        .call(g => g.select(".tick:last-of-type text").clone()
-        .attr("x", 3)
-        .attr("text-anchor", "start")
-        .attr("font-weight", "bold")
-        .text(data3.y))
-
-  svg3.append("g")
-      .call(xAxis);
-  
-  svg3.append("g")
-      .call(yAxis);
-
-
-});
-
-
-d3.tsv('https://gist.githubusercontent.com/edlsto/9c6c9c57326b82c8a61c339d3ef82798/raw/7677c77793f4fba175789340befa2b88d360a317/snow-totals-resorts.tsv', function(data) {
-  var columns = data.columns.slice(1).map(d => parseTime(d))
-  var series = data.map( (d, i, columns) => {
-    return {
-name: d.name,
-values: data.columns.slice(1).map(k => +d[k])
-}
-  })
-  var data =  {
-    y: "# inches",
-series: series,
-dates: columns
-}
-
-
-  var x = d3.scaleTime()
-    .domain(d3.extent(data.dates))
-    .range([margin.left, width - margin.right])
-  var y = d3.scaleLinear()
-    .domain([0, d3.max(data.series, d => d3.max(d.values))]).nice()
-    .range([height - margin.bottom, margin.top])
-  var line = d3.line()
-    .defined(d => !isNaN(d))
-    .x((d, i) => x(data.dates[i]))
-    .y(d => y(d))
-          .curve(d3.curveCatmullRom);
-  
-  xAxis = g => g
-    .attr("transform", `translate(0,${height - margin.bottom})`)
-    .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0).tickFormat(d3.timeFormat("%b")))
-;
-    yAxis = g => g
-    .attr("transform", `translate(${margin.left},0)`)
-    .call(d3.axisLeft(y))
-    .call(g => g.select(".domain").remove())
-    .call(g => g.select(".tick:last-of-type text").clone()
-        .attr("x", 3)
-        .attr("text-anchor", "start")
-        .attr("font-weight", "bold")
-        .text(data.y))
-
-  svg4.append("g")
-      .call(xAxis);
-  svg4.append("g")
-      .call(yAxis);
-
-  var path = svg4.append("g")
-      .attr("fill", "none")
-      .attr("stroke", "#ddd")
-      .attr("stroke-width", 1.5)
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-linecap", "round")
-      .selectAll("path")
-    .data(data.series)
-   .enter()
-   .append("path")
-      .style("mix-blend-mode", "multiply")
-      .attr("d", d => line(d.values))
-      .attr("id", (d, i) => {
-      return "years" + i
-      })
-    
-  d3.select('path#years2').attr("stroke", "steelblue")
-  
-  const dot = svg4.append("g")
-  dot.append("circle")
-      .attr("r", 2.5);
-  dot.append("text")
-      .style("font", "10px sans-serif")
-      .attr("text-anchor", "middle")
-      .attr("y", -8);
-    dot.attr("transform", "translate(562.7506549178377,203.6)")
-    .attr("id", "thisyear")
-    dot.select("text").text("Vail")
-  svg4.call(hover, path);
-  return svg4.node();
-
-function hover(svg, path) {
-  svg
-      .style("position", "relative");
-  if ("ontouchstart" in document) svg
-      .style("-webkit-tap-highlight-color", "transparent")
-      .on("touchmove", moved)
-      .on("touchstart", entered)
-      .on("touchend", left)
-  else svg
-      .on("mousemove", moved)
-      .on("mouseenter", entered)
-      .on("mouseleave", left);
-  const dot = svg.append("g")
-      .attr("display", "none");
-  dot.append("circle")
-      .attr("r", 2.5);
-  dot.append("text")
-      .style("font", "10px sans-serif")
-      .attr("text-anchor", "middle")
-      .attr("y", -8);
-  function moved() {
-    d3.event.preventDefault();
-    const ym = y.invert(d3.event.layerY);
-    const xm = x.invert(d3.event.layerX);
-    const i1 = d3.bisectLeft(data.dates, xm, 1);
-    const i0 = i1 - 1;
-    const i = xm - data.dates[i0] > data.dates[i1] - xm ? i1 : i0;
-    const s = data.series.reduce((a, b) => Math.abs(a.values[i] - ym) < Math.abs(b.values[i] - ym) ? a : b);
-    path.attr("stroke", d => d === s ? 'steelblue' : "#ddd").filter(d => d === s).raise();
-    dot.attr("transform", `translate(${x(data.dates[i])},${y(s.values[i])})`);
-    dot.select("text").text(s.name)
-    d3.select('#thisyear').remove()
-  }
-  function entered() {
-    path.style("mix-blend-mode", null).attr("stroke", "#ddd");
-    dot.attr("display", 'null');
-  }
-  function left() {
-    path.style("mix-blend-mode", "multiply").attr("stroke", null);
-    dot.attr("transform", "translate(562.7506549178377,203.6)")
-      d3.select('path#years2').attr("stroke", "steelblue")
-    dot.select("text").text("Vail")
-  }
-}
-})
-
-
