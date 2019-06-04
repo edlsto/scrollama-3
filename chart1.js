82696e8983bc3beb5ecf456d570bc7f3e4e4897b
Ed Stoner <edwardlstoner@gmail.com>
2019-06-03 18:20:23 -0600

    write copy

--- chart1.js
+++ chart1.js
@@ -4,7 +4,7 @@ var dateFormatter = d3.timeFormat("%b. %d");
 
 
 
-var margin = {left: 100, right: 30, top: 20, bottom: 20}
+var margin = {left: 50, right: 30, top: 20, bottom: 20}
 
 
     var width = 800 - margin.left - margin.right;
@@ -17,7 +17,7 @@ function responsivefy(svg) {
         height = parseInt(svg.style("height")),
         aspect = width / height;
             var fontSize = parseInt(container.style("width")) < 768 ? "20px" : "10px";
-
+          
 
     // add viewBox and preserveAspectRatio properties,
     // and call resize so that svg resizes on inital page load
